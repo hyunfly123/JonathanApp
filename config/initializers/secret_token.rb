a19683cf7094e03a4d6a0e6f09ec34e18a36ddd1
Jonathan Hyun <hyunfly123@hotmail.com>
2012-09-18 15:08:43 -0700

    first upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JonathanApp::Application.config.secret_token = '254188f775ef8f5144b776b6f72f7ba6a331e98192507281b5cbb4ac9295c94c48c4ea8875c51204478be2ba1bcdf23a3f86f4d273b85e31cd2ac6dd5e07cc8c'
